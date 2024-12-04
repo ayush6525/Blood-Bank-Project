83939b543236dd280bec12c19e3019dccbeee4f0
Ayush Dubey <ayushdubey729@gmail.com>
2024-12-05 00:05:47 +0530

    connect database

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const colors = require('colors')
+const morgan = require('morgan')
+const cors = require('cors')
+const connectDB = require('./config/db.js')
+//dot config
+dotenv.config();
 
+//mongodb connection
+connectDB();
 // rest object
 const app = express();
+
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
 
 //routes
 // 1 test route
 app.use('/api/v1/test', require("./routes/testRoutes.js"));
 
 // PORT
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // LISTEN
 app.listen(PORT, () => {
-    console.log("Node server Running");
+    console.log(
+        `Node server Running In ${process.env.DEV_MODE} MOde On Port ${process.env.PORT}`
+        .bgBlue.white
+    );   
 });
